140baaca5ae4562de2131989db11e8fe73c0de71
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2024-12-27 13:34:28 -0500

    retexture stone tools to copper tools

---
+++ src/main/java/net/bluebunnex/peanutbutter/PeanutbutterClient.java
@@ -0,0 +1,34 @@
+package net.bluebunnex.peanutbutter;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.item.Item;
+import net.modificationstation.stationapi.api.client.event.texture.TextureRegisterEvent;
+import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
+import net.modificationstation.stationapi.api.util.Identifier;
+
+public class PeanutbutterClient {
+
+    @EventListener
+    public void registerTextures(TextureRegisterEvent event) {
+
+        Item.STONE_SWORD.setTextureId(
+                Atlases.getGuiItems().addTexture(Identifier.of(Peanutbutter.NAMESPACE, "item/copper_sword")).index
+        );
+
+        Item.STONE_PICKAXE.setTextureId(
+                Atlases.getGuiItems().addTexture(Identifier.of(Peanutbutter.NAMESPACE, "item/copper_pickaxe")).index
+        );
+
+        Item.STONE_AXE.setTextureId(
+                Atlases.getGuiItems().addTexture(Identifier.of(Peanutbutter.NAMESPACE, "item/copper_axe")).index
+        );
+
+        Item.STONE_SHOVEL.setTextureId(
+                Atlases.getGuiItems().addTexture(Identifier.of(Peanutbutter.NAMESPACE, "item/copper_shovel")).index
+        );
+
+        Item.STONE_HOE.setTextureId(
+                Atlases.getGuiItems().addTexture(Identifier.of(Peanutbutter.NAMESPACE, "item/copper_hoe")).index
+        );
+    }
+}
