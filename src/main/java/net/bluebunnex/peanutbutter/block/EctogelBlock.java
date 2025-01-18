c2e9d3c193557b75f09951c8f9e9985efa884b9b
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-18 18:59:04 -0500

    improve ectogel (translucent + sneaking stops speed)

--- src/main/java/net/bluebunnex/peanutbutter/block/EctogelBlock.java
+++ src/main/java/net/bluebunnex/peanutbutter/block/EctogelBlock.java
@@ -1,20 +1,28 @@
 package net.bluebunnex.peanutbutter.block;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.IceBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.template.block.TemplateBlock;
+import net.modificationstation.stationapi.api.template.block.TemplateTranslucentBlock;
 import net.modificationstation.stationapi.api.util.Identifier;
 
-public class EctogelBlock extends TemplateBlock {
+public class EctogelBlock extends TemplateTranslucentBlock {
 
     public EctogelBlock(Identifier identifier) {
-        super(identifier, Material.SPONGE);
+        super(identifier, 0, Material.SPONGE, false);
 
         this.setSoundGroup(EctogelBlock.GRAVEL_SOUND_GROUP);
     }
+
+    // stolen from IceBlock
+    @Environment(EnvType.CLIENT)
+    public int getRenderLayer() { return 1; }
 
     @Override
     public Box getCollisionShape(World world, int x, int y, int z) {
@@ -23,6 +31,9 @@ public class EctogelBlock extends TemplateBlock {
 
     @Override
     public void onEntityCollision(World world, int x, int y, int z, Entity entity) {
+
+        if (entity.isSneaking())
+            return;
 
         double speed = MathHelper.sqrt(entity.velocityX * entity.velocityX + entity.velocityZ * entity.velocityZ);
 
